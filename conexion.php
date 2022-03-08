7e905b593715efcb0034727e32460be49764e744
Cristian Insuasti <ceim243@gmail.com>
2022-03-08 18:03:46 -0500

    new BD

--- conexion.php
+++ conexion.php
@@ -1,15 +1,3 @@
 <?php
-	//MySQL
 	$con=mysqli_connect("localhost","root","","");
-	//SQL	
-	/*$serverName = "localhost, 1433"; //serverName\instanceName, portNumber (por defecto es 1433)
-	$connectionInfo = array( "Database"=>"pruebaandresmartinez", "UID"=>"root", "PWD"=>"Felipe10100");
-	$con = sqlsrv_connect( $serverName, $connectionInfo);
-
-	if( $con ) {
-     	echo "Conexión establecida.<br />";
-	}else{
-     	echo "Conexión no se pudo establecer.<br />";
-     	die( print_r( sqlsrv_errors(), true));
-	}*/
 ?>
