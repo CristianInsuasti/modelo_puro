d8422de5b2882de7774c942b2e80ec34c2645cab
Cristian Insuasti <ceim243@gmail.com>
2022-03-08 18:02:57 -0500

    new BD

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 	//MySQL
-	$con=mysqli_connect("localhost","root","","pruebaandresmartinez");
+	$con=mysqli_connect("localhost","root","","");
 	//SQL	
 	/*$serverName = "localhost, 1433"; //serverName\instanceName, portNumber (por defecto es 1433)
 	$connectionInfo = array( "Database"=>"pruebaandresmartinez", "UID"=>"root", "PWD"=>"Felipe10100");
